670a0decf9a41afe2e1e03054564f39325368d32
Peter Hoff <peter.hoff@duke.edu>
2022-10-08 07:27:47 -0400

    Updated to work on both linux and mac

--- app.R
+++ app.R
@@ -5,7 +5,7 @@
 #### 
 #### Shiny app to render and serve a searchable webpage of Rmarkdown notes.
 ####
-#### How to use:
+#### How to:
 #### 1. Put this app in a directory, say `/Users/me/Notebook` 
 #### 2. Make a subdirectory `www`.
 #### 3. Make a subdirectory `content`.
@@ -20,21 +20,41 @@
 #### ---- 
 render<-function(){  
 
-  ## md files
-  mdFiles<-system("stat -l -t '%FT%T' content/*.md",intern=TRUE) 
-  mark<-regexpr("content",mdFiles) 
-  mark2<-sapply(gregexpr("\\.",mdFiles),max) 
-  mdNames<-substring(mdFiles,mark+8,mark2-1) 
-  mdTimes<-gsub("T"," ",substring(mdFiles,mark-20,mark-2))
+  ## get os to find stat version
+  linux<-( system("uname",intern=TRUE) == "Linux" )
+
+
+
+  ## md files 
+  if(linux){ mdFiles<-system("stat -c'%y %n' content/*.md",intern=TRUE)} else
+           { mdFiles<-system("stat -l -t '%FT%T' content/*.md",intern=TRUE)}
+
+  ## md names
+  mdNames<-tools::file_path_sans_ext(basename(mdFiles))
+
+  ## md times 
+  if(linux){ mdTimes<-substring(mdFiles,1,19) } else
+           { mark<-regexpr("content",mdFiles) 
+             mdTimes<-gsub("T"," ",substring(mdFiles,mark-20,mark-2)) } 
   mdTimes<-strptime(mdTimes,"%Y-%m-%d %H:%M:%S")
 
+
+
   ## html files
-  hFiles<-system("stat -l -t '%FT%T' www/*.html",intern=TRUE)
-  mark<-regexpr("www",hFiles)
-  hNames<-substring(hFiles,mark+4,nchar(hFiles)-5)                  
-  hTimes<-gsub("T"," ",substring(hFiles,mark-20,mark-2))
+  if(linux){ hFiles<-system("stat -c'%y %n' www/*.html",intern=TRUE)} else
+           { hFiles<-system("stat -l -t '%FT%T' www/*.html",intern=TRUE) } 
+
+  ## html names 
+  hNames<-tools::file_path_sans_ext(basename(hFiles))
+
+  ## html times 
+  if(linux){ hTimes<-substring(hFiles,1,19) } else
+           { mark<-regexpr("www",hFiles)
+	     hTimes<-gsub("T"," ",substring(hFiles,mark-20,mark-2)) }
   hTimes<-strptime(hTimes,"%Y-%m-%d %H:%M:%S")
- 
+
+
+
   ## remove defunct html files
   rmhtml<-match(setdiff(hNames,mdNames),hNames) 
   if(length(rmhtml)>0)
@@ -44,34 +64,26 @@ render<-function(){
     hNames<-hNames[ -rmhtml ] 
   }
 
-  ## see which files need to be rendered
-
-  #### match md to html
-  frend<-match(mdNames,hNames)  
-
-  ### check to see if md has been updated since last render
-  frend[!is.na(frend)]<-unlist( 
-        mapply( function(x,y){ difftime(x,y)>0 }, 
-        mdTimes[!is.na(frend)],
-        hTimes[frend[!is.na(frend)]] )   )
- 
-  ### if not matched then need to render 
-  frend[is.na(frend)]<-TRUE 
 
 
-  ## read yaml, render things that need to be rendered 
-  YAML<-list() 
-  for(i in seq_along(mdFiles)) 
-  {
+  ## identify files in need of rendering
+   frend<-mdTimes>hTimes[match(mdNames,hNames)]
+   frend[is.na(frend)]<-TRUE
+
+  ## render things that need to be rendered 
+  for(i in which(frend))
+  { 
+    # rmarkdown wont render R-code in .md files 
     mdfname<-paste0("content/",mdNames[i],".md") 
-    YAML[[i]]<-rmarkdown::yaml_front_matter(mdfname) 
-    if(frend[i]){  
-      tf<-paste0("content/",mdNames[i],".Rmd") 
-      system(paste0("cp ",mdfname," ",tf))
-      rmarkdown::render(tf,output_file=paste0(mdNames[i],".html"),
-                        output_dir="www",envir=new.env())
-      system(paste0("rm ",tf)) 
-    }
+    tmpfile<-paste0("content/",mdNames[i],".Rmd") 
+    system(paste0("cp ",mdfname," ",tmpfile))
+    rmarkdown::render(tmpfile,output_file=paste0(mdNames[i],".html"),
+                      output_dir="www" ,envir=new.env())  
+      #`new.env` is needed so that code chunks wont modify the environment
+      # in which this command is running. However, variables in command
+      # environment may still be visible from the code chunks. 
+       
+      system(paste0("rm ",tmpfile))  
   }
 
 }
